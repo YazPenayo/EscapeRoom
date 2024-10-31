4117657e845858196c059beec7f0b498f1480fc7
yazpenayo <yazmin.penayo@hotmail.com>
2024-10-31 15:01:04 -0300

    MVC y conexión a la base de datos

---
+++ www/Models/database.php
@@ -0,0 +1,17 @@
+<?php
+$db_host = getenv("MYSQL_HOST");
+$db_user = getenv("MYSQL_USER");
+$db_password = getenv("MYSQL_PASSWORD");
+$db_name = getenv("MYSQL_DB");
+
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if($conn->connect_error) {
+    die("connection failed");
+}/*else {
+    echo "Conexión exitosa";
+}*/
+
+if (!$conn->set_charset("utf8mb4")) {
+    die("Error loading character set utf8mb4: " . $conn->error);
+}
